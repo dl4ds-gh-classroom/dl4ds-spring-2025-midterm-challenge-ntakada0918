b6f4bc588530b22bad5e248f7d18bacc428b7939
Namika Takada <168774971+ntakada0918@users.noreply.github.com>
2025-03-26 21:36:56 -0400

    Adding part 2

---
+++ Sophisticated_cnn.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import os
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm  # For progress bars
+import wandb
+import json
+
+################################################################################
+# Model Definition (Simple Example - You need to complete)
+# For Part 1, you need to manually define a network.
+# For Part 2 you have the option of using a predefined network and
+# for Part 3 you have the option of using a predefined, pretrained network to
+# finetune.
+################################################################################
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(ResNet18, self).__init__()
+        self.model = torchvision.models.resnet18(pretrained=False, num_classes=100)
+    
+    def forward(self, x):
+
+        return self.model(x)
+
+################################################################################
+# Define a one epoch training function
+################################################################################
+def train(epoch, model, trainloader, optimizer, criterion, CONFIG):
+    """Train one epoch, e.g. all batches of one epoch."""
+    device = CONFIG["device"]
+    model.train()  # Set the model to training mode
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    # put the trainloader iterator in a tqdm so it can printprogress
+    progress_bar = tqdm(trainloader, desc=f"Epoch {epoch+1}/{CONFIG['epochs']} [Train]", leave=False)
+
+    # iterate through all batches of one epoch
+    for i, (inputs, labels) in enumerate(progress_bar):
+
+        # move inputs and labels to the target device
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        ### TODO - Your code here
+        optimizer.zero_grad()
+        output = model(inputs)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        _, predicted = torch.max(output, 1)
+
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+        progress_bar.set_postfix({"loss": running_loss / (i + 1), "acc": 100. * correct / total})
+
+    train_loss = running_loss / len(trainloader)
+    train_acc = 100. * correct / total
+    return train_loss, train_acc
+
+################################################################################
+# Define a validation function
+################################################################################
+def validate(model, valloader, criterion, device):
+    """Validate the model"""
+    model.eval() # Set to evaluation
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad(): # No need to track gradients
+        
+        # Put the valloader iterator in tqdm to print progress
+        progress_bar = tqdm(valloader, desc="[Validate]", leave=False)
+
+        # Iterate throught the validation set
+        for i, (inputs, labels) in enumerate(progress_bar):
+            
+            # move inputs and labels to the target device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+            progress_bar.set_postfix({"loss": running_loss / (i+1), "acc": 100. * correct / total})
+
+    val_loss = running_loss/len(valloader)
+    val_acc = 100. * correct / total
+    return val_loss, val_acc
